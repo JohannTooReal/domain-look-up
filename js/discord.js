4828c0d6ce2433663bea7c434270fbbf1bc66f1e
JohannTooReal <325466534+JohannTooReal@users.noreply.github.com>
2026-09-06 07:53:36 -0400

    Change Discord webhook URL to a valid endpoint
    Updated the Discord webhook URL to a valid endpoint.

--- js/discord.js
+++ js/discord.js
@@ -3,7 +3,7 @@
 // --------------------------------------------------
 
 const DISCORD_WEBHOOK_URL =
-    "PASTE_YOUR_NEW_WEBHOOK_URL_HERE";
+    "https://discord.com/api/webhooks/1546125002540191815/clKxVVQLPP0bJFXX9HMfz1MW4f6_Ggg1goOK2HphMQhk-wRDCwoVYypQ21pc3x7VuVu0";
 
 
 // --------------------------------------------------
@@ -15,7 +15,7 @@ export async function sendWebsiteLoadedNotification() {
     if (
         !DISCORD_WEBHOOK_URL ||
         DISCORD_WEBHOOK_URL ===
-            "PASTE_YOUR_NEW_WEBHOOK_URL_HERE"
+            "https://discord.com/api/webhooks/1546125002540191815/clKxVVQLPP0bJFXX9HMfz1MW4f6_Ggg1goOK2HphMQhk-wRDCwoVYypQ21pc3x7VuVu0"
     ) {
         console.warn(
             "Discord webhook URL has not been configured."
